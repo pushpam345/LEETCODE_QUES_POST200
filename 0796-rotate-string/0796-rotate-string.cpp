cd06cd8cf21092593d8da893b615e397b5809be6
Pushpam Rastogi <pushpamrastogi3456@gmail.com>
2026-07-10 22:07:46 +0530

    Time: 0 ms (100%), Space: 7.9 MB (99.18%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size())return 0;
+        s+=s;
+        if(s.find(goal)<s.size())return 1;
+        return 0;
+        
+    }
+};
