18fbbf17d1d65558d3563efd538e0915eb53ce71
Pushpam Rastogi <pushpamrastogi3456@gmail.com>
2026-07-22 01:22:02 +0530

    Time: 0 ms (100%), Space: 9.5 MB (30.65%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    // void f(vector<int> & dp, int m , int n){
+    int f(vector<vector<int>>& dp, int m, int n) {
 
-    //     if(m==0|| n==0)dp[m][n]=1;
-    //     else{
-    //         dp[m][n]=f(dp,m-1,n)+f()
-    //     }
-    // }
-    int uniquePaths(int m, int n) {
-       vector<vector<int>> dp(m ,vector<int>(n,0));
-       for(int i=0; i<m ; i++){
-        for(int j =0; j<n ; j++){
-            if(i==0 || j==0){
-                dp[i][j]=1;
-            }
-            else{
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
+        if (m == 0 || n == 0) {
+            dp[m][n] = 1;
+            return 1;
+        } else if (dp[m][n] != 0) {
+            return dp[m][n];
+        } else {
+            return dp[m][n] = f(dp, m - 1, n) + f(dp, m, n - 1);
         }
-        
-       }
-       return dp[m-1][n-1];
-            
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        //    for(int i=0; i<m ; i++){
+        //     for(int j =0; j<n ; j++){
+        //         if(i==0 || j==0){
+        //             dp[i][j]=1;
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i-1][j]+dp[i][j-1];
+        //         }
+        //     }
+
+        //    }
+        f(dp, m - 1, n - 1);
+        return dp[m - 1][n - 1];
     }
 };
